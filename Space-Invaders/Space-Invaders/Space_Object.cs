685fe5fb4d99cb6aa54e6983f24d3c62cb878227
weidc1 <weidc1@student.op.ac.nz>
2016-05-20 11:55:35 +1200

    Created Space_Object

---
+++ Space-Invaders/Space-Invaders/Space_Object.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Invaders
+{
+    class Space_Object
+    {
+
+        public Space_Object()
+        {
+
+        }
+    }
+}
